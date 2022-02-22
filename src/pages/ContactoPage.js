da9bc2277811d67d479ceca1d8fa47b931a92f4e
Andrea Acosta <acostaandrea2012@gmail.com>
2022-02-22 15:49:23 -0300

    proyecto2

--- src/pages/ContactoPage.js
+++ src/pages/ContactoPage.js
@@ -27,7 +27,7 @@ const ContactoPage = (props) => {
     e.preventDefault();
     setMsg('');
     setSending(true)
-    const response = await axios.post('http://localhost:3000/api/contacto', formData);
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/contacto`, formData);
     setSending(false);
     setMsg(response.data.message);
     if (response.data.error === false) {
